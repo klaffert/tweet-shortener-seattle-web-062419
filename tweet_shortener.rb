bbfa1a2320a7cd71b3351ed523bc4bd15676d7ff
Kelliann Lafferty <kell10192@gmail.com>
2019-06-19 23:18:09 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -44,9 +44,10 @@ def selective_tweet_shortener(tweet)
 end 
 
 def shortened_tweet_truncator(tweet)
-  if word_substituter(tweet).length > 140 
-    word_substituter(tweet)[0..140] + '...'
+  if selective_tweet_shortener(tweet).length > 140 
+    selective_tweet_shortener(tweet)[0..140] + '...'
   else 
     tweet 
   end 
 end 
+
