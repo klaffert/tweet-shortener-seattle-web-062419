82e4ff5c427fbb242bd9af584db72f74ae4af6c3
Kelliann Lafferty <kell10192@gmail.com>
2019-06-19 23:16:46 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -33,6 +33,8 @@ def bulk_tweet_shortener(array)
   end
 end
 
+# shortens tweets that are more than 140 characters
+# does not shorten tweets that are less than 130 characters
 def selective_tweet_shortener(tweet)
   if tweet.length > 140 
     word_substituter(tweet)
